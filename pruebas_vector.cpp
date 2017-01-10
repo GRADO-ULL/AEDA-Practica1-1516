a02f23d9502cf7b811adee70761d67be18dc57b6
JosueTC94 <alu0100763492@ull.edu.es>
2017-01-10 10:54:48 +0000

    First Commit

---
+++ pruebas_vector.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdlib.h>
+#include "comun.hpp"
+#include "vector.hpp"
+
+int main()
+{
+  vector v1;
+  TDATO dato_abuscar;
+  //vector v2(10);
+  cout << "1.- Se crea el vector. La dimension debe ser 0 y y no debe tener elementos:" << endl;
+  cout << "Dimension: " << v1.get_dimension() << endl;
+  cout << "Elementos: ";
+  v1.mostrar_elementos();
+  cout << "2.- Se insertan 10 elementos en el vector. La dimension debe ser 10 y no debe estar vacio el vector:" << endl;
+  v1.dimensionar(10);
+  for(int i=0;i<10;i++)
+  {
+    v1.insertar(i,i);
+  }
+  cout << "Dimension: "<< v1.get_dimension() << endl;
+  cout << "Elementos: ";
+  v1.mostrar_elementos();
+  cout << endl;
+  cout << "3.- Elimino el elemento en la posicion 3 del vector:" << endl;
+  v1.eliminar_xpos(3);
+  cout << "Elementos: ";
+  v1.mostrar_elementos();
+  cout << endl;
+  cout << "Dimension: " << v1.get_dimension() << endl;
+  cout << "4.- Elimino la celda del vector que contiene el dato 7:" << endl;
+  v1.eliminar_dato(7);
+  cout << "Elementos: ";
+  v1.mostrar_elementos();
+  cout << endl;
+  cout << "Dimension: " << v1.get_dimension() << endl;
+  cout << endl;
+  cout << "6.- Buscar un dato."<< endl;
+  cout << "Introduzca dato:";
+  cin >> dato_abuscar;
+  v1.buscar_dato(dato_abuscar);
+  cout << endl;
+  cout << "7.- Operaciones con el vector:" << endl;
+  v1.operaciones();
+  cout << endl;
+  return 0;
+}
